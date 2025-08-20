c78ac8806c3e9ae65f7b6b9f9bee18223da7124f
Gildman Laércio <laerciokarlagabm@gmail.com>
2025-08-20 05:39:10 -0300

    index.js fixed

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,36 +1,26 @@
+const clearButton = document.getElementById('clear-form');
+const form = document.getElementById('form-content');
 const mortgageAmount = document.getElementById('mortgage-amount');
 const mortgageTerm = document.getElementById('mortgage-term');
 const interestRate = document.getElementById('interest-rate');
-const mortgageType = document.querySelector('input[type="radio"][name="mortgage-type"]');
+const mortgageType = document.querySelectorAll('input[type="radio"][name="mortgage-type"]');
 const emptyState = document.querySelector('.empty-state');
 const filledState = document.querySelector('.filled-state');
 
 function sendButton(event) {
   event.preventDefault()
 
-  if (mortgageAmount === "" || mortgageTerm === "" || interestRate === "" || mortgageType.checked === false) {
-    alert('Por favor preencha os campos solicitados');
-    return;
-  } else {
-    emptyState.style.display = "none";
-    filledState.style.display = "grid";
+  if(!form.checkValidity()) {
+    form.reportValidity();
     return;
   }
+
+  emptyState.style.display = "none";
+  filledState.style.display = "grid";
 }
 
-// const clearForm = document.getElementById('clear-form');
-
-function clearForm(event) {
-  event.preventDefault()
-  
-  if(clearForm) {
-    mortgageAmount.value = "";
-    mortgageTerm.value = "";
-    interestRate.value = "";
-    mortgageType.checked = false;
-    
-    emptyState.style.display = "grid";
-    filledState.style.display = "none";
-    return;
-  }
-}
+clearButton.addEventListener('click', () => {
+  form.reset();
+  emptyState.style.display = "grid";
+  filledState.style.display = "none";
+});
