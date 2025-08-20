bc8f7bc07ef080287e78ebba04998e9567be297d
Gildman Laércio <laerciokarlagabm@gmail.com>
2025-08-19 22:21:14 -0300

    index.js incremented

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,30 +1,36 @@
-const clearClass = document.getElementById('clear-class');
 const mortgageAmount = document.getElementById('mortgage-amount');
 const mortgageTerm = document.getElementById('mortgage-term');
 const interestRate = document.getElementById('interest-rate');
+const mortgageType = document.querySelector('input[type="radio"][name="mortgage-type"]');
 const emptyState = document.querySelector('.empty-state');
 const filledState = document.querySelector('.filled-state');
 
 function sendButton(event) {
   event.preventDefault()
 
-  if (mortgageAmount || mortgageTerm || interestRate === "") {
+  if (mortgageAmount === "" || mortgageTerm === "" || interestRate === "" || mortgageType.checked === false) {
     alert('Por favor preencha os campos solicitados');
+    return;
   } else {
     emptyState.style.display = "none";
-    filledState.style.display = "flex";
+    filledState.style.display = "grid";
+    return;
   }
 }
 
-function clearForm() {
+// const clearForm = document.getElementById('clear-form');
 
-
+function clearForm(event) {
+  event.preventDefault()
+  
   if(clearForm) {
     mortgageAmount.value = "";
     mortgageTerm.value = "";
     interestRate.value = "";
-
+    mortgageType.checked = false;
+    
     emptyState.style.display = "grid";
     filledState.style.display = "none";
+    return;
   }
 }
